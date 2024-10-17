24681737aec97b4c9a6128a5f6ff02ef47c100b0
Manny2023 <120243119+Manny2023@users.noreply.github.com>
2024-10-16 20:36:30 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,24 @@
-<script>
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('worker.js?'+Math.random()+'')
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Evitar que el banner se muestre automáticamente
+  e.preventDefault();
+  deferredPrompt = e;
+
+  // Muestra un botón de instalación personalizado
+  const installButton = document.querySelector('#installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    // Muestra el banner de instalación
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('El usuario aceptó la instalación');
+      } else {
+        console.log('El usuario rechazó la instalación');
+      }
+      deferredPrompt = null;
+    });
   });
-}
-</script>
+});
